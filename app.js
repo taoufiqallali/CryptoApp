e434cc101513126040f0ed5ebd7e3c4797302d71
Taoufiq Allali <taoufiqallali0@gmail.com>
2024-11-24 23:17:45 +0100

    added plain text cypher text headers

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let btn = document.getElementById('btn');
 let encryptBtn = document.getElementById('encryptBtn');
 let decryptBtn = document.getElementById('decryptBtn');
+let plainText = document.getElementById('plain-text');
+let cypherText = document.getElementById('cypher-text');
 
 function leftClick(){
 
     btn.style.left = '0%';
     encryptBtn.style.color = 'white';
     decryptBtn.style.color = '';
+    plainText.textContent = 'Plain text';
+    cypherText.textContent = 'Cypher text';
 
 }
 
@@ -15,5 +19,7 @@ function rightClick(){
     btn.style.left = '50%';
     decryptBtn.style.color = 'white';
     encryptBtn.style.color = 'black';
+    plainText.textContent = 'Cypher text';
+    cypherText.textContent = 'Plain text';
 
 }
